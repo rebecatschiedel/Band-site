04a35f9f5ffba7bb3a281710c445c433079ad4da
rebecatschiedel <rebecatcmg@gmail.com>
2022-06-05 11:37:45 -0700

    Hero component

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Hero.scss";
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      <h1 className="hero__title">The Bees Knees</h1>
+    </section>
+  );
+}
